74d3f02b238781ae41d2050fbfa19f2a78123907
johnnichola <johnnichola21@gmail.com>
2026-04-26 21:25:12 -0600

    added scroll animation

---
+++ script.js
@@ -0,0 +1,22 @@
+
+// this is for scrolling to specific section
+function onScrollToSection(event)
+{
+    event.preventDefault();
+
+    const _targetElem = document.getElementById("arcade-anthology");
+    const _navH = 72; // current height of the nav
+    
+    // get the top position of the arcade-anthology element
+    const _elemPos = _targetElem.getBoundingClientRect().top;
+
+    // set offset so it gives padding on the arcade-anthology element
+    const _offsetPos = _elemPos + window.pageYOffset - _navH;
+
+    window.scrollTo(
+        {
+            top: _offsetPos,
+            behavior: "smooth"
+        }
+    );
+}
